a49a55a796fa355ccff0d2b3afac98e8ff7503b4
Jörg Wasmeier <j.wasmeier@faba.de>
2017-10-12 15:56:42 +0200

    Permission Check

--- src/FabaNodeMediator.ts
+++ src/FabaNodeMediator.ts
@@ -3,7 +3,9 @@ import FabaEvent from "@fabalous/core/FabaEvent";
 import {FabaNodeCommand} from "./FabaNodeCommand";
 export class FabaNodeMediator extends FabaCoreMediator{
 
-    addCommand(event:any, command: typeof FabaNodeCommand): void {
+    addCommand(event:any, command:any, check?): void {
+        if (check && !check()) return;
+
         super.addCommand(event, command);
     }
 }
