76103c9740f818673152e339340a2ca959bccd36
Jörg Wasmeier <j.wasmeier@faba.de>
2017-10-14 15:09:15 +0200

    Update deps

--- src/FabaNodeMediator.ts
+++ src/FabaNodeMediator.ts
@@ -1,6 +1,4 @@
 import FabaCoreMediator from "@fabalous/core/FabaCoreMediator";
-import FabaEvent from "@fabalous/core/FabaEvent";
-import {FabaNodeCommand} from "./FabaNodeCommand";
 export class FabaNodeMediator extends FabaCoreMediator{
 
     addCommand(event:any, command:any, check?): void {
